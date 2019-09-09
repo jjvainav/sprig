4d5052b7475ccb85227234276ca9186b493798d1
Jeremy Vainavicz <jjvainav@gmail.com>
2019-09-09 13:26:43 -0400

    Initial setup.

---
+++ jest.config.base.js
@@ -0,0 +1,19 @@
+﻿module.exports = {
+    globals: {
+        "ts-jest": {
+            tsConfig: "<rootDir>/test/tsconfig.json"
+        }
+    },
+    roots: [
+        "<rootDir>/test"
+    ],
+    transform: {
+        "^.+\\.(js|ts)$": "ts-jest"
+    },
+    transformIgnorePatterns: [
+        "node_modules/?!(@sprig)"
+    ],
+    testRegex: "((\\.|/)(test))\\.ts$",
+    moduleFileExtensions: ["js", "ts"],
+    verbose: true
+}
