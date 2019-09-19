e39c69046feef9bafa67a055f18ab9b6af1f0b25
Jeremy Vainavicz <jjvainav@gmail.com>
2019-09-19 07:17:59 -0400

    Added random string utility.

---
+++ packages/random-string/src/index.ts
@@ -0,0 +1,23 @@
+import * as base62 from "base62";
+import * as crypto from "crypto";
+
+export = function randomString(length: number): string {
+    // this is borrowed from: https://gist.github.com/aseemk/3095925
+    const maxNum = Math.pow(62, length);
+    const numBytes = Math.ceil(Math.log(maxNum) / Math.log(256));
+
+    if (numBytes === Infinity) {
+        throw new Error("Length too large; caused overflow: " + length);
+    }
+
+    do {
+        var num = 0
+        const bytes = crypto.randomBytes(numBytes);
+        
+        for (let i = 0; i < bytes.length; i++) {
+            num += Math.pow(256, i) * bytes[i];
+        }
+    } while (num >= maxNum);
+
+    return base62.encode(num);
+}
